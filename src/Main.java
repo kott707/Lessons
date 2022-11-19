c0a2b03a8c1f6b6e2f27ef9697adf713f574e5d9
kott707 <234782@mail.ru>
2022-11-19 22:53:21 +0500

    something

---
+++ src/Main.java
@@ -0,0 +1,68 @@
+
+
+
+class Main {
+    public static void main(String[] args) {
+
+        //имеем 2конструктора, имя + счетчик по умолчанию, имя + счетчик
+        Counter counter = new Counter("Clock",50);
+
+        //положительное число увеличит, отрицательное уменьшит, 0 - предупреждение
+        counter.increaseCounter(1);
+
+        //рассчет количества четных и не четных чисел
+        counter.checkEvenness(1, 10);
+
+    }
+}
+
+class Counter {
+    private int counter;
+    private String nameCounter;
+
+    {
+        counter = 0;
+    }
+
+    public Counter( String nameCounter, int counter) {
+        this.counter = counter;
+        this.nameCounter = nameCounter;
+    }
+
+    public Counter(String nameCounter) {
+        this.nameCounter = nameCounter;
+    }
+
+    //метод увеличит счетчик на любое положительное число, сообщит если 0, убавит если отрицательное
+    //не делал return, так как в данной задачи нет применения возвращаемого по return, а только вывод в консоль
+    public void increaseCounter(int setNumber){
+
+        if (setNumber > 0) {
+            counter +=setNumber;
+            System.out.println("Счетчик увеличен на "+setNumber+" Текущее значение = "+counter);
+        } else if (setNumber == 0){
+            System.out.println("Вы ввели "+setNumber+" и счетчик не изменился [ "+counter+" ]");
+        } else {
+            counter -=setNumber;
+            System.out.println("Счетчик уменьшен на "+setNumber+" Текущее значение = "+counter);
+        }
+    }
+
+    public void checkEvenness(int fromNumber, int toNumber){
+
+        int evenNumberCount = 0;
+        int oddNumberCount = 0;
+
+        for (int i = fromNumber; i <= toNumber; i++){
+            if(i%2 == 0) {
+                evenNumberCount++;
+            }else {
+                oddNumberCount++;
+            }
+        }
+        System.out.println("Нечетные числа: "+oddNumberCount+"\nЧетные числа: "+evenNumberCount);
+    }
+
+}
+
+
